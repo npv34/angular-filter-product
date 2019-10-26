5342ad7dfc73363497942062cd112280ba7e0d51
npv34 <phanvanluan611996@gmail.com>
2019-10-26 22:50:24 +0700

    update filter product

--- src/app/shared/input-filter/input-filter.component.ts
+++ src/app/shared/input-filter/input-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-input-filter',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-filter.component.css']
 })
 export class InputFilterComponent implements OnInit {
-
+  keyword: string;
+  @Output() filterKeyWord = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
+  }
+  getKeyWord(): void {
+    this.filterKeyWord.emit(this.keyword);
   }
 
 }
